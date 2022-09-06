bb2c6ce623723e58e31dd055175c0ff6863d7a38
Laptop Andrey <sinko02@inbox.ru>
2022-09-06 21:26:03 +0300

    Добавил ввод двух целых чисел

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "sdt.h"
 #include <stdio.h>
+#include <iostream>
 
-
+using namespace std;
 
 int main(){
-    
+    int a, b;
+    cout << "Enter two integer numbers:";
+    cin >> a >> b;
+    return 0;
 }
 
