34d0bd8a0b5455b981ba1305123e423e9af09c3f
Laptop Andrey <sinko02@inbox.ru>
2022-09-07 13:36:53 +0300

    Добавил умножение двух чисел

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ using namespace std;
 
 int main(){
     int a, b;
-    cout << "Enter two integer numbers:";
+    cout << "Enter two integer numbers: ";
     cin >> a >> b;
     cout << "a + b = " << a + b << endl;
-    cout << "a - b = " << a - b;
+    cout << "a - b = " << a - b << endl;
+    Cout << "a * b = " << a * b;
     return 0;
 }
 
