10cf28dcb9c17303e0550da5df187c321db38ad2
Laptop Andrey <sinko02@inbox.ru>
2022-09-07 13:03:01 +0300

    Добавил разность введенных чисел

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main(){
     int a, b;
     cout << "Enter two integer numbers:";
     cin >> a >> b;
-    cout << "a + b = " << a + b;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b;
     return 0;
 }
 
